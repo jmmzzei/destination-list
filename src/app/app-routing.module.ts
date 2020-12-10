9d4749d9592e161435e6e288504aa22bc1f9ec6b
José María <josemmazzei@gmail.com>
2020-12-10 17:51:19 -0300

    Add Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DestinationComponent } from './destination/destination.component';
 import { DetailDestinationComponent } from './detail-destination/detail-destination.component';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: DestinationComponent },
-  { path: 'destination', component: DetailDestinationComponent },
+  { path: 'home', component: ListComponent },
+  { path: 'detail', component: DetailDestinationComponent },
 ];
 
 @NgModule({
